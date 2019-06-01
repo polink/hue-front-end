77727427778f1e32eaacf5c892727279739a619e
Karl Polintan <kf.polntan@gmail.com>
2019-06-01 16:39:10 -0700

    Placeholder Login in components/login.js

--- src/components/login.js
+++ src/components/login.js
@@ -4,25 +4,23 @@ class Login extends React.Component {
     render(){
         <div>
             <h1>Login</h1>
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit="{this.handleSubmit}">
                 <input
                     name='username'
                     placeholder='username'
                     type='text'
-                    value={this.state.username}
-                    onChange={this.handleChange}
+                    value='{this.state.username}'
+                    onChange='{this.handleChange}'
                 />
-                {type !== 'login' ? emailJSX : undefined}
                 <input
                     name='password'
                     placeholder='password'
                     type='password'
-                    value={this.state.password}
-                    onChange={this.handleChange}
+                    value='{this.state.password}'
+                    onChange='{this.handleChange}'
                 />
-                <button type='submit'>{type}</button>
+                <button type='submit'>Login</button>
             </form>
-            <Link to='/signup'> Sign up to our application</Link>
-        </div>;
+        </div>
     }
 }
